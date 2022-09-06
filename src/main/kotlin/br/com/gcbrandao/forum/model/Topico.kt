036b0ed58de02f722fdbcc2d07f3cdbca8c556ae
gcbrandao <gcbrandao@gmail.com>
2022-09-06 17:55:06 -0300

    adicionando dependencias de JPA e mapeando as entities

--- src/main/kotlin/br/com/gcbrandao/forum/model/Topico.kt
+++ src/main/kotlin/br/com/gcbrandao/forum/model/Topico.kt
@@ -1,15 +1,26 @@
 package br.com.gcbrandao.forum.model
 
 import java.time.LocalDateTime
+import javax.persistence.*
 
+@Entity
 data class Topico(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     val titulo: String,
     val mensagem: String,
     val dataCriacao: LocalDateTime = LocalDateTime.now(),
+
+    @ManyToOne
     val curso: Curso,
+
+    @ManyToOne
     val autor: Usuario,
+
+    @Enumerated(value = EnumType.STRING)
     val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
+
+    @OneToMany(mappedBy = "topico")
     val respostas: List<Resposta> = ArrayList()
 
 )
