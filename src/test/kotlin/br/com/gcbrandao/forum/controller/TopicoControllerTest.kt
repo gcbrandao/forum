4f732210838442702fd51c75ff3519b1da2396ba
gcbrandao <gcbrandao@gmail.com>
2022-09-21 12:19:16 -0400

    adicionando teste de api com mockMvc

--- src/test/kotlin/br/com/gcbrandao/forum/controller/TopicoControllerTest.kt
+++ src/test/kotlin/br/com/gcbrandao/forum/controller/TopicoControllerTest.kt
@@ -1,4 +1,73 @@
 package br.com.gcbrandao.forum.controller
 
+import br.com.gcbrandao.forum.config.JWTUtil
+import br.com.gcbrandao.forum.model.Role
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TopicoControllerTest {
+
+    @Autowired
+    private lateinit var webApplicationContext: WebApplicationContext
+
+    @Autowired
+    private lateinit var jwtUtil: JWTUtil
+
+    private lateinit var mockMvc: MockMvc
+
+    private var token: String? = null
+
+    companion object {
+        private const val RECURSO = "/topicos/"
+        private const val RECURSO_ID = RECURSO.plus("%s")
+    }
+
+    @BeforeEach
+    fun setUp() {
+
+        token = gerarToken()
+
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply<DefaultMockMvcBuilder?>(
+                SecurityMockMvcConfigurers.springSecurity()
+            )
+            .build()
+    }
+
+    @Test
+    fun `deve retornar cod 400 quando chamada sem atuntencicao`() {
+        mockMvc.get(RECURSO).andExpect { status { is4xxClientError() } }
+    }
+
+    @Test
+    fun `deve retornar cod 200 quando chamada com atuntencicao`() {
+        mockMvc.get(RECURSO) {
+            headers { token?.let { this.setBearerAuth(it) } }
+        }.andExpect { status { is2xxSuccessful() } }
+
+    }
+
+    @Test
+    fun `deve retornar cod 200 quando chamada topico por id com token`() {
+        mockMvc.get(RECURSO_ID.format("1")) {
+            headers { token?.let { this.setBearerAuth(it) } }
+        }.andExpect { status { is2xxSuccessful() } }
+
+    }
+
+
+    private fun gerarToken(): String? {
+
+        val authorities = mutableListOf(Role(1, "LEITURA_ESCRITA"))
+        return jwtUtil.generateToken("gcbrandao@gmail.com", authorities)
+    }
 }
